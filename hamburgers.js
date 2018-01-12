48a2767c0045cd034038d27a2c21b9a9b252443d
Luan Albineli <crosscountrybrasil@gmail.com>
2018-01-12 00:49:44 -0200

    Added the image url to the burger list

--- hamburgers.js
+++ hamburgers.js
@@ -4,20 +4,31 @@ module.exports = [
 		"name": "Traditional Burger",
 		"description": "Fresh 100% beef burger with our signature sauce, garnished with a dill pickle slice, lettuce, tomato and onion",
 		"ingredients": ["Beef burger", "Sauce", "Pickles", "Lettuce", "Tomato", "Onion"],
-		"price": 15.5
+		"price": 15.5,
+		"imageUrl": "https://media-cdn.tripadvisor.com/media/photo-s/0e/74/f0/15/3-brasseurs-burger-with.jpg"
 	},
 	{
 		"id": 2,
 		"name": "Bacon & Cheese Burger",
 		"description": "Fresh 100% beef burger with our signature sauce, topped with crisp bacon and melted cheese, garnished with a dill pickle slice, lettuce, tomato and onion",
 		"ingredients": ["Beef burger", "Bacon", "Sauce", "Pickles", "Lettuce", "Tomato", "Onion"],
-		"price": 18.5
+		"price": 18.5,
+		"imageUrl": "https://s3.amazonaws.com/btoimage/prism-thumbnails/uploads/2016/12/05/20161201-3brasseurs-11.jpg-resize-_opacity_100-frame_bg_color_FFF-h_2500-gravity_center-q_70-preserve_ratio_true-w_1400_.jpg"
 	},
 	{
 		"id": 3,
 		"name": "Brie Blue Burger",
 		"description": "Creamy Brie blue cheese and sweet caramelized onions on top of our hand-pressed 100% beef burger. Garnished with fresh arugula and a drizzle of balsamic reduction",
 		"ingredients": ["Beef burger", "Brie blue cheese", "Caramelized onions", "Arugula"],
-		"price": 20
+		"price": 20,
+		"imageUrl": "https://media-cdn.tripadvisor.com/media/photo-s/07/ea/46/2e/les-3-brasseurs.jpg"
+	},
+	{
+		"id": 4,
+		"name": "BBQ pulled pork sandwich",
+		"description": "BBQ-braised pulled pork with melted Swiss cheese and crispy battered onions on a baguette",
+		"ingredients": ["BBQ-braised pulled pork", "Swiss cheese", "Crispy onions"],
+		"price": 19.8,
+		"imageUrl": "https://ederepente50.files.wordpress.com/2017/06/brasseurs.jpg?w=604"
 	}
 ];
